b72a1691ced02c4c9785c3931267547d0247071b
kimdobbang <153525545+kimdobbang@users.noreply.github.com>
2023-12-12 02:16:50 +0900

    [Bronze V] Title: 등록, Time: 108 ms, Memory: 13784 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.println("8");
         System.out.println("kimdobbang");
     }
