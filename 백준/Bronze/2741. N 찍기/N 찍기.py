b48aa36f7d3c83a91b355bbe4cea9555d147b991
dobbang <153525545+kimdobbang@users.noreply.github.com>
2023-12-24 07:15:50 +0900

    [Bronze V] Title: N 찍기, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.py
@@ -0,0 +1,6 @@
+N = int(input())
+
+for i in range(1, N+1): # (시작값i, 끝값N) 1 이상 N+1 미만의 숫자 범위를 생성 123
+# # for i in range(N): # (시작값0, 끝값N-1) 012
+# for i in range(1, N): # (시작값1, 끝값N-1) 12
+    print(i)
