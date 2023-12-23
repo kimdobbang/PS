b4e9660acab651ab6b47f05954b738f2b04a6f2d
dobbang <153525545+kimdobbang@users.noreply.github.com>
2023-12-23 17:07:05 +0900

    Update 윤년.java

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt(); // 1 <= year <= 4000 이라서 int로 받아도 무리 없다.
+        int year = sc.nextInt(); // 1 <= year <= 4000 이라서 int로 받아도 무방
         //연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때
-        if((year%4==0 && year%100 !=0)||year%400 ==0){
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println("1");
-        }else{
+        } else {
             System.out.println("0");
         }
     }
