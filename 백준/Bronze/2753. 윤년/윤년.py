47a26798c96f1766c49558e17809c42e5edab1b9
dobbang <153525545+kimdobbang@users.noreply.github.com>
2023-12-23 17:06:39 +0900

    Update 윤년.py

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,6 +1,14 @@
 y = int(input())
 
-# # 오답
+# 제출_231223
+if (y % 4 == 0 and y % 100 != 0) or y % 400 ==0:
+    print("1")
+else:
+    print("0")
+
+# 오답
+# &를 쓰시고 싶으시다면 괄호를 이용하는 방법도 있습니다. 비트연산자와 혼동되기 때문에 보통은 윗분 말씀대로 and를 이용합니다.
+
 # if y % 4 == 0 & y % 100 != 0 :
 #     print('1')
 # elif y % 400 == 0 :
@@ -9,7 +17,7 @@ y = int(input())
 #     print('0')
 
 
-# # 정답1
+# 정답1
 # if (y % 4 == 0) & (y % 100 != 0) :
 #     print('1')
 # elif y % 400 == 0 :
@@ -17,10 +25,11 @@ y = int(input())
 # else :
 #     print('0')
 
-# 정답2
-if y % 4 == 0 and y % 100 != 0 :
-    print('1')
-elif y % 400 == 0 :
-    print('1')
-else :
-    print('0')
+
+# # 정답2
+# if y % 4 == 0 and y % 100 != 0 :
+#     print('1')
+# elif y % 400 == 0 :
+#     print('1')
+# else :
+#     print('0')
