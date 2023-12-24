7724f0c9b1f65719617533b6587baac8e5621f58
dobbang <153525545+kimdobbang@users.noreply.github.com>
2023-12-24 13:49:42 +0900

    Update 고양이.java

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,9 +1,14 @@
+//escape 문자열에 대한 문제(주석 해제하면 오류 발생)
+//인텔리제이는 출력할 내용을 paste 하면 자동으로 escape처리를 해줌
 
 public class Main {
     public static void main(String[] args) {
+////        System.out.println(""안녕하세요"");
+//        System.out.println("\"안녕하세요\"");
+////        System.out.println("\n");
+//        System.out.println("\\n");
+////        System.out.println("\");
+//        System.out.println("\\");
         System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
-    }
-}
