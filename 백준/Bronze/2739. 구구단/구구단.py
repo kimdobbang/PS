1210ef222f8703f7fb4c31f0b3076fd212d1c96b
kim doi <153525545+kimdobbang@users.noreply.github.com>
2023-12-28 00:09:45 +0900

    Update 구구단.py

--- 백준/Bronze/2739. 구구단/구구단.py
+++ 백준/Bronze/2739. 구구단/구구단.py
@@ -1,12 +1,20 @@
-N = int(input())
-
-# # i 범위를 1부터 9까지 지정. i는 이미 1로 시작하므로 변수 초기화 불필요
+# i 범위를 0부터 8까지 지정후 i에 1씩 더해짐 따라서 i= 1 ~ 9 되어 위의 range(1,10)과 동일 효과
+#N = int(input())
+#for i in range(9):
+#    i += 1
+#    result = f'{N} * {i} = {N * i}'
+#    print(result)
+    
+# #(다른 방법 도전) i 범위를 1부터 9까지 지정. i는 이미 1로 시작하므로 변수 초기화 불필요
+# N = int(input())
 # for i in range(1, 10):
 #     result = f'{N} * {i} = {N * i}'
 #     print(result) 
 
- # i 범위를 0부터 8까지 지정후 i에 1씩 더해짐 따라서 i= 1 ~ 9 되어 위의 range(1,10)과 동일 효과
-for i in range(9):
-    i += 1
-    result = f'{N} * {i} = {N * i}'
-    print(result) 
+    
+#남의 풀이 (이렇게 하는게 가장 좋은 듯 range(9)하니 변수초기화 해야하고 덜 직관적)
+import sys
+n = int(sys.stdin.readline())
+for i in range(1, 10):
+    print('{} * {} = {}'.format(n,i, n*i))
+#print(f'{n} * {i} = {n*i}')
