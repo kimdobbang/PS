56b09441741bb27294ba3923ebf385f7c6053e77
kim doi <kimdoistory@gmail.com>
2024-01-05 22:41:24 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -1,11 +1,5 @@
 n = int(input())
 
-for i in range(n):
+for i in range(n) :
     i += 1
     print("*"*i)
-    
-# for i in range(n):
-#     print("*"*(i+1))
-
-# for i in range(1, n+1):
-#     print("*"*i)
