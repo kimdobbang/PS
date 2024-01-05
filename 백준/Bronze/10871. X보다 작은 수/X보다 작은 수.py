d5d4060b3be568755839da5b8c46790e5cc225d1
kim doi <kimdoistory@gmail.com>
2024-01-05 22:40:00 +0900

    [Bronze V] Title: X보다 작은 수, Time: 44 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,15 +1,8 @@
 import sys
 
 n, x = map(int, sys.stdin.readline().split())
-num = list(map(int, sys.stdin.readline().split()))
+a = list(map(int, sys.stdin.readline().split()))
 
-for i in range(n):
-    if num[i] < x:
-        print(num[i], end = ' ')
-
-
-# 내가 푼건데,,,위의 풀이가 더 맞는 것 같다. 아래는 n을 활용하지 않았음ㅜ 그리고 range()를 쓰지 않고 그냥 num이라고 써도 되네..??
-# for i in num:
-#     if i < x:
-#         print(i, end = ' ')
-# 공백으로 구분하여 출력해야하는데 처음에 end=' '를 몰라서 한 줄씩 출력 됐다.
+for i in a:
+    if i < x:
+        print(i)
