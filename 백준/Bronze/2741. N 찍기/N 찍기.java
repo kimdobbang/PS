2bd38ff57d304e9c2213e90c888f96d748e73839
kim doi <kimdoistory@gmail.com>
2024-01-05 22:44:12 +0900

    [Bronze V] Title: N 찍기, Time: 1056 ms, Memory: 32628 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i < N + 1; i++) {
             System.out.println(i);
         }
     }
