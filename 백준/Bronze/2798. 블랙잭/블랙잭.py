7a4efbacd5e7079214212f1c143bb890db4bb671
kim doi <kimdoistory@gmail.com>
2024-01-06 17:58:11 +0900

    [Bronze II] Title: 블랙잭, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,13 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+l = list(map(int,sys.stdin.readline().split()))
+
+max_sum = 0
+
+for i in range(len(l)):
+    for j in range(i+1, len(l)):
+        for k in range(j+1, len(l)):
+            temp_sum = l[i] + l[j] + l[k]
+            if temp_sum <= m and temp_sum > max_sum:
+                max_sum = temp_sum
+print(max_sum)
