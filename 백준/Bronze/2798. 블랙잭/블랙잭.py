28ef9e9ed03624020ec22904ded16b5960aff1ec
kim doi <kimdoistory@gmail.com>
2024-01-06 22:05:39 +0900

    블랙잭.py 업데이트

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -5,7 +5,7 @@ l = list(map(int,sys.stdin.readline().split()))
 max_sum = 0
 
 for i in range(len(l)):
-    for j in range(i+1, len(l)):
+    for j in range(i+1, len(l)-1):
         for k in range(j+1, len(l)):
             temp_sum = l[i] + l[j] + l[k]
             if temp_sum <= m and temp_sum > max_sum:
