8063f537817e95387d3e40431c8829381c92626f
kim doi <kimdoistory@gmail.com>
2024-01-09 02:08:10 +0900

    Update 개수 세기.py

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -1,9 +1,13 @@
 n = int(input())
 numbers = list(map(int, input().split()))
+# Numlist = Numstr.split(' ') -> 처음본다?
 v = int(input())
 count = 0
 
-for i in range(n):
-    if numbers[i] == v:
+# 처리
+for i in numbers:
+    if i == v:
         count += 1
 print(count)
+
+# for 문 돌리지않고 위 처리부를 주석처리 후 print(numbers.count(v))해도 됨
