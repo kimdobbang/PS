1c1569418767ecfab1372e8c67279bffd052eba7
kim doi <kimdoistory@gmail.com>
2024-01-21 21:53:18 +0900

    Update 검증수.py

--- 백준/Bronze/2475. 검증수/검증수.py
+++ 백준/Bronze/2475. 검증수/검증수.py
@@ -1,4 +1,14 @@
-res = 0
-for n in list(map(int, input().split())):
-    res += n**2
-print(res%10)
+N = list(map(int, input().split()))
+
+temp = []
+for n in N:
+    temp.append(n**2)
+print(sum(temp) % 10)
+
+# 꼭 list가 필요햇던것은 아니다. 
+# 그런데 무의식적으로 평소에 자주 쓰던 방법을 가져와 풀게되는 것 같다. 더 좋은 법 이 있어도ㅜ
+
+# temp = 0
+# for n in list(map(int, input().split())):
+#     temp += n**2
+# print(temp % 10)
