5512e0a2d52ea75be80382897e5807e0477a482a
kim doi <kimdoistory@gmail.com>
2024-01-26 04:08:38 +0900

    Update 세 수.py

--- 백준/Bronze/10817. 세 수/세 수.py
+++ 백준/Bronze/10817. 세 수/세 수.py
@@ -1,7 +1,11 @@
-A, B, C = map(int,(input().split()))
-list = [A, B, C]
+list = list(map(int,(input().split())))
 
 print(sorted(list)[1])
+
+# A, B, C = map(int,(input().split()))
+# list = [A, B, C]
+
+# print(sorted(list)[1])
 
 # A, B, C를 리스트에 담아 Sorted 후 index[1]
 # 2번째 큰 정수만 찾으면 되지,,ABC가 각각 누군지 중요치 않은 것 같음
