f480b9c112ec17559b1ab7e25db5862b579bb85f
kim doi <kimdoistory@gmail.com>
2024-02-29 02:45:02 +0900

    [Silver V] Title: 색종이, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -2,33 +2,34 @@
 # 색종이는 10 * 10
 # a: 왼쪽변 ~ 도화지 왼쪽 변 사이 거리
 # b: 색종이 아래쪽 변 ~ 도화지 아래쪽 변 사이 거리
-# 색종이영역
-# (3,17) (13, 17)
-# (3, 7) (13, 7)
-# (a, b+10) (a+10, b+10)
-# (a, b) (a+10, b)
 
 N = int(input())
-paper = [[0] * 100 for _ in range(100)]
+paper = [[False] * 100 for _ in range(100)]
 
-cnt = 0
 
-# 색종이 영역에 1 입력
-for i in range(N):
+for _ in range(N):
+    # 1) 색종이 영역에 1 표기
     a, b = map(int, input().split())
     for i in range(a, a+10):
         for j in range(b, b+10):
-            paper[i][j] = 1
+            paper[i][j] = True
 
 
 # # 확인
 # for i in paper:
 #     print(*i)
 
-# 1을 카운트
-for i in range(100):
-    for j in range(100):
-        if paper[i][j] == 1:
-            cnt += 1
+# 2) 1을 카운트 => 넓이
+# ans = 0
+# for i in range(100):
+#     for j in range(100):
+#         if paper[i][j]:
+#             ans += 1
+#
+# print(ans)
 
-print(cnt)
+# 2-2) 1카운트 하는 다른 방법
+ans = 0
+for  lst in paper:
+    ans += sum(lst)
+print(ans)
