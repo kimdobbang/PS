3b8bbd9ffa5d74ae3cb01f72fe2621343467f802
kim doi <kimdoistory@gmail.com>
2024-03-02 14:18:35 +0900

    Update 기상캐스터.py

--- 백준/Silver/10709. 기상캐스터/기상캐스터.py
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.py
@@ -1,12 +1,13 @@
-H, W = map(int, input().split()) # 구역
-arr = [[-1] * W for _ in range(H)]
+H, W = map(int, input().split())  # 구역
 cast = [input() for _ in range(H)]  # 문자열 H개 주어짐
+arr = [[-1] * W for _ in range(H)]  # 출력할 문자열
 
 for i in range(H):
-    c = False  # c 만나면 True로 바뀌고, 조건문에서 True일때 앞의 값에 1씩 더해준 값을 새로운 배열에 넣어줌.
+    c = False  # c 만나면 True로 바뀌고, 조건문에서 True일때 앞의 값에 1씩 더해준 값을 현재 위치에 출력
     for j in range(W):
-        if c == True:
+        if c:
             arr[i][j] = arr[i][j-1] + 1
+            
         if cast[i][j] == 'c':
             arr[i][j] = 0
             c = True
