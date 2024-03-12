2e3a79cda0ea6c4f0b2ec530857354744a1ac94a
kim doi <kimdoistory@gmail.com>
2024-03-12 22:55:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 676 ms, Memory: 65076 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,14 +1,9 @@
 from collections import deque
 import sys
-input = sys.stdin.readline
-
-N, M = map(int, input().split())
-lst = [[] for _ in range(N+1)]
-q = deque()
 
 
 def bfs(s):
-    q.append(s)
+    q = deque([s])
     visited[s] = True
     while q:
         now = q.popleft()
@@ -18,6 +13,11 @@ def bfs(s):
                 visited[next] = True
 
 
+input = sys.stdin.readline
+N, M = map(int, input().split())
+lst = [[] for _ in range(N+1)]
+visited = [False] * (N + 1)
+
 # 인접리스트
 for i in range(1, M+1):
     u, v = map(int, input().split())
@@ -26,7 +26,6 @@ for i in range(1, M+1):
 
 # bfs dfs 모든 곳 다 탐색하기 위해
 # bfs 호출한 횟수가 연결요소의 개수
-visited = [False] * (N + 1)
 cnt = 0
 for i in range(1,N+1):
     if not visited[i]:
