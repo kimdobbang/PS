49f4eb0642c3983ce1df724e3d3bdb413ae71481
kim doi <kimdoistory@gmail.com>
2024-03-26 03:08:10 +0900

    [Silver II] Title: 부분수열의 합, Time: 276 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,3 +1,4 @@
+# 부분수열의 합
 def solutoin(idx, cnt, sum_v):  # idx: 탐색횟수, cnt: 부분수열크기, sum_v: 부분수열 합
     global ans
 
@@ -8,14 +9,14 @@ def solutoin(idx, cnt, sum_v):  # idx: 탐색횟수, cnt: 부분수열크기, su
         if sum_v == S:  # 원소의 합이 S와 같다면
             ans += 1  # 부분수열 갯수 1개 카운트
             # return  # 난 여기서 계속 에러나서 틀렸음
-        return  # dix == N일 떄 종료해야지
+        return  # dix == N일 땐 S와 무관하게 무조건 종료해야지
 
     solutoin(idx + 1, cnt + 1, sum_v + lst[idx])  # 부분수열 원소로 선택 한 경우
     solutoin(idx + 1, cnt, sum_v)  # 부분수열 원소로 선택 안한 경우
 
 
 N, S = map(int, input().split())
-lst = list(map(int,input().split()))
+lst = list(map(int, input().split()))
 ans = 0  # 부분수열 갯수
 solutoin(0, 0, 0)
 print(ans)
