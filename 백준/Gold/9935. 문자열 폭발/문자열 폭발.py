e298428dbc2ad8696130313f2a2ed8f1abad6ffd
kim doi <kimdoistory@gmail.com>
2024-04-04 23:37:22 +0900

    [Gold IV] Title: 문자열 폭발, Time: 964 ms, Memory: 48104 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,16 @@
+str = list(input().rstrip())
+check = list(input().rstrip())
+
+stack = []
+for i in str:
+    stack.append(i)
+    if stack[-len(check):] == check: # 폭탄문자열과 같은지 뒤부터 len(폭탄문자열) 만큼 확인하여 삭제
+        del stack[-len(check):]
+
+# 반복문 종료 후 출력
+if stack:
+    for i in stack:
+        print(*i,end='')
+    # print(''.join(stack))
+else:
+    print("FRULA")
