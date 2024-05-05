dd859eebe455382f5dff522ccafcc87dd6d40d89
kim doi <thisisdoemail@gmail.com>
2024-05-05 20:40:13 +0900

    [Bronze I] Title: 세로읽기, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 세로읽기
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 띄어 쓰기가 없는 2차원 배열 생성 및 입력
+        char[][] arr = new char[5][15];
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                arr[i][j] = line.charAt(j);
+            }
+        }
+
+        // 확인
+        for (int j = 0; j < 15; j++) {
+            for (int i = 0; i < 5; i++) {
+                if (arr[i][j] == 0){ // 캐릭터의 기본값이 아스키코드 0
+                    continue;
+                }
+                System.out.print(arr[i][j]);
+            }
+        }
+    }
+}
