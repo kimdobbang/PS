29e3c0fcc8ee33565457eba9439fa0b1bcac6255
kim doi <thisisdoemail@gmail.com>
2024-06-25 19:03:24 +0900

    [Bronze IV] Title: Absolutely, Time: 136 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/26500. Absolutely/Absolutely.java
@@ -0,0 +1,23 @@
+// Absolutely
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            double x = Double.parseDouble(st.nextToken());
+            double y = Double.parseDouble(st.nextToken());
+            double diff = Math.round(Math.abs(x - y) * 10) / (double) 10;
+            sb.append(String.format("%.1f", diff)).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
