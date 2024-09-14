20dbc4f95d49cf278cc185a4f6da570b21a7e0dc
kim doi <thisisdoemail@gmail.com>
2024-09-14 18:23:32 +0900

    [Bronze II] Title: 단어의 개수, Time: 124 ms, Memory: 14016 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,12 +1,30 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st = new StringTokenizer(br.readLine());
-		System.out.println(st.countTokens());
+		int cnt = 0;
+		int pre_str = 32;
+		int now_str;
+
+		while (true) {
+			// 현재문자
+			now_str = System.in.read();
+
+			// 현재 공백 && 이전문자 공백아님
+			if (now_str == 32 && pre_str != 32) {
+				cnt++;
+			}
+
+			// 현재 문자 줄바꿈 && 이전 문자 공백 아님(공백일땐 앞의 조건문에서 cnt+1처리)
+			else if (now_str == 10) {
+				if (pre_str != 32) {
+					cnt++;
+				}
+				break;
+			}
+			// 이전문자에 현재문자
+			pre_str = now_str;
+		}
+		System.out.println(cnt);
 	}
 }
