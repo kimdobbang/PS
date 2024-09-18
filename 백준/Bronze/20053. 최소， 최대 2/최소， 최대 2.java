93f81fa9a651f95ce8ddb4083ae57517ece8474f
kim doi <thisisdoemail@gmail.com>
2024-09-18 16:57:33 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 800 ms, Memory: 246476 KB -BaekjoonHub

---
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
@@ -0,0 +1,40 @@
+// 최소, 최대 2
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine()); //테케
+		
+		for (int t = 0; t < T; t++) {
+			int N = Integer.parseInt(br.readLine()); // 정수 갯수
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int min = Integer.MAX_VALUE;
+			int max = Integer.MIN_VALUE;
+			for(int n = 0; n < N ; n++) {
+				int num = Integer.parseInt(st.nextToken());
+				if (num > max) {
+					max = num;
+				}
+				if (num < min) {
+					min = num;
+				}
+			}
+			// 마지막 테스트 케이스에서는 개행을 넣지 않기
+            if (t == T - 1) {
+                sb.append(min).append(" ").append(max);
+            } else {
+                sb.append(min).append(" ").append(max).append("\n");
+            }
+		}
+		System.out.print(sb);
+	}
+
+}
