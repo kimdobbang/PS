aae8cf73f5e4372eafd8e069a738372c2adfcfa0
kim doi <thisisdoemail@gmail.com>
2024-09-21 11:15:46 +0900

    [Silver IV] Title: 듣보잡, Time: 272 ms, Memory: 28948 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,43 +1,45 @@
-// 듣보잡
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		HashMap<String, Integer> map = new HashMap<>();
-		TreeSet<String> resultSet = new TreeSet<>();
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		HashSet<String> noListen = new HashSet<>();
+		HashSet<String> noSee = new HashSet<>();
 
-		// map으로 카운팅
-		for (int i = 0; i < N + M; i++) {
-			String name = br.readLine();
-			map.put(name, map.getOrDefault(name, 0)+1);
+		for (int i = 0; i < n; i++) {
+			noListen.add(br.readLine());
 		}
 
-		// 2인 애들만 정렬하는 셋에 넣음
-		for (String name : map.keySet()) {
-			if (map.get(name) == 2) {
-				resultSet.add(name);
+		for (int i = 0; i < m; i++) {
+			noSee.add(br.readLine());
+		}
+
+		ArrayList<String> answer = new ArrayList<>();
+		for (String s : noListen) {
+			if (noSee.contains(s)) {
+				answer.add(s);
 			}
 		}
 
-		// 출력
-		sb.append(resultSet.size()).append("\n");
-		for (String name : resultSet) {
-			sb.append(name).append("\n");
-		}
-		System.out.println(sb);
+		Collections.sort(answer);
 
+		sb.append(answer.size()).append("\n");
+		for (String s : answer) {
+			sb.append(s).append("\n");
+		}
+
+		System.out.println(sb);
 	}
 }
